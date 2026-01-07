011bd0481751c9d6e5e606211bdb61a6d64c5d71
vendul0g <a.navarro@um.es>
2026-01-07 14:04:12 +0100

    Updated setup_docker to include Debian installation

--- setup_docker.sh
+++ setup_docker.sh
@@ -78,6 +78,14 @@ else
         curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
         chmod a+r /etc/apt/keyrings/docker.asc
         echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian bookworm stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+    elif [ "$OS" = "Debian" ]; then
+        echo_info "Instalando Docker para Debian..."
+        apt-get install -y ca-certificates curl
+        install -m 0755 -d /etc/apt/keyrings
+        curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+        chmod a+r /etc/apt/keyrings/docker.asc
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" \
+  | tee /etc/apt/sources.list.d/docker.list > /dev/null
     else
         echo_info "Instalando Docker para Ubuntu..."
         apt-get install -y ca-certificates curl
